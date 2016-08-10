9201c44157a2db947011f4da7fad753e83cebac4
thiagoasd <thiago_teixeira4@yahoo.com.br>
2016-08-09 23:23:41 -0300

    Separated groups

--- Python/CDparser.py
+++ Python/CDparser.py
@@ -1,16 +1,35 @@
+import re
+import sys
 from os import listdir;
 from os.path import isdir, join;
-import re, sys;
+from openpyxl import Workbook;
+from openpyxl.cell import get_column_letter;
 
 path = str(sys.argv[1]);
-print path;
+book = Workbook();
+sheet = book.active;
 
+row_number = 1;
 for f in listdir(path):
+    column_number = 1;
     cpath = join(path, f);
-    print cpath;
     if isdir(cpath):
-        print f + "lol";
         m = re.search('(\[.*\])(.*)(\[.*\])', cpath);
-        print(m.group(0));
+        if m is None or len(m.groups()) < 3:
+            print("Error " + cpath);
+            continue;
+        cell = sheet.cell(row=row_number, column=column_number);
+        cell.value = m.group(1);
+        print(cell.value);
+        cell = sheet.cell(row=row_number, column=column_number + 1);
+        cell.value = m.group(2);
+        print(cell.value);
+        cell = sheet.cell(row=row_number, column=column_number + 2);
+        cell.value = m.group(3);
+        print(cell.value);
+        row_number += 1;
+        sheet.column_dimensions[get_column_letter(row_number -1)].width = 50;
 
-# TODO SEPARATE GROUPS
+book.save("test.xlsx");
+
+# TODO SEPARATE FLACS
